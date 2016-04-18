f397486478168afbf62093924dfea9c912078b30
Jaegeuk Kim <jaegeuk@kernel.org>
2016-04-18 17:07:44 -0400

    f2fs: issue cache flush on direct IO
    Under direct IO path with O_(D)SYNC, it needs to set proper APPEND or UPDATE
    flags, so taht f2fs_sync_file can make its data safe.
    
    Acked-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -673,6 +673,9 @@ next_block:
 					err = reserve_new_block(&dn);
 			} else {
 				err = __allocate_data_block(&dn);
+				if (!err)
+					set_inode_flag(F2FS_I(inode),
+							FI_APPEND_WRITE);
 			}
 			if (err)
 				goto sync_out;
@@ -1758,8 +1761,12 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb,
 							get_data_block_dio);
 #endif
 out:
-	if (err < 0 && (rw & WRITE))
-		f2fs_write_failed(mapping, offset + count);
+	if (err < 0 && (rw & WRITE)) {
+		if (err > 0)
+			set_inode_flag(F2FS_I(inode), FI_UPDATE_WRITE);
+		else if (err < 0)
+			f2fs_write_failed(mapping, offset + count);
+	}
 
 	trace_f2fs_direct_IO_exit(inode, offset, count, rw, err);
 
